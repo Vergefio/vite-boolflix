058e03372e37cc79b897801597913f86e72d71ae
Vergefio <riccardods@live.it>
2022-11-21 23:30:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+
+<template>
+  <div class="container">
+    ciao
+  </div>
+</template>
+
+
+
+<style lang="scss">
+  @use './styles/general';
+</style>
